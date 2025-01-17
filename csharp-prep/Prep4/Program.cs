4530785f8e972afae9950ea5b6d4b542f8f400c2
zwhaymond <zwhaymond@byui.edu>
2025-01-17 13:15:36 -0700

    Pre assignments

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // List to store user's numbers.
+        List<int> numList = new List<int>();
+        int lastNum = -1;
+
+        // A loop to allow the user to add numbers until they add zero.
+        while (lastNum != 0)
+        {
+            Console.Write("Enter number: ");
+            lastNum = int.Parse(Console.ReadLine());
+            
+            numList.Add(lastNum);
+        }
+
+        // Remove the last number (zero) as it's not part of the calculations.
+        numList.RemoveAt(numList.Count - 1);
+
+        // Find total and average of the list.
+        int total = 0;
+        foreach (int num in numList)
+        {
+            total += num;
+        }
+
+        double average = (double)total / numList.Count;
+
+        // Display results.
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+
+        // Find and display the highest number.
+        int maxNum = int.MinValue;
+        foreach (int num in numList)
+        {
+            if (num > maxNum)
+            {
+                maxNum = num;
+            }
+        }
+        Console.WriteLine($"The biggest number is: {maxNum}");
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Generate a magic number.
+        Random random = new Random();
+        int magicNum = random.Next(1, 11); // Generates a random number between 1 and 10
+        int guess = 0;
+
+        while (magicNum != guess)
+        {
+            // Ask user for a guess.
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            // If statement determining if it's too high, low, or is the correct guess.
+            if (magicNum == guess)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (magicNum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
