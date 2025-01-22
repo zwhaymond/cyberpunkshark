e0c7e305850f554abc47b566e313b949665174de
zwhaymond <zwhaymond@byui.edu>
2025-01-21 19:58:22 -0700

    Code for Learning activity 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+         Job job1 = new Job();
+         job1._jobTitle = "Software Engineer";
+         job1._company = "Hex Headquarters";
+         
+
+         Job job2 = new Job();
+         job2._jobTitle = "Mechanical Engineer";
+         job2._company = "Faulty Factory";
+         
+
+        Resume resume1 = new Resume();
+        resume1._name = "Billy Bucket";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
